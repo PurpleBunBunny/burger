7a4fa197f9587ef9e2d905b8cf860463cc5663d0
PurpleBunny <michael.l33.1994@gmail.com>
2018-11-18 19:43:58 -0800

    final commit

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Classic Burger');
+INSERT INTO burgers (burger_name) VALUES ('Big Mac');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Krabby Patty', TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Quarter Punder w/ Cheese', TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Double Cheese Burger', TRUE);
